10fbb82c6ae25edb48f9091332318d713597baee
David Erwin <davidjerwin@gmail.com>
2015-04-06 12:34:04 -0400

    Adding golang puppet module

---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+stage { "prepare":
+  before => Stage["main"],
+}
+
+class setup {
+  exec { "update-aptitude":
+    command => "/usr/bin/yum update -y",
+  }
+}
+
+class {
+  "setup":
+    stage => prepare;
+  "golang":;
+}
